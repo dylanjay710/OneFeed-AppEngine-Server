1606801061ed16c1a2c18922ae25229d0b7d5624
dylanjay710 <dylanjay710@gmail.com>
2017-03-29 05:09:26 -0400

    app is setup to test post requests and user account data

--- main.py
+++ main.py
@@ -52,28 +52,62 @@ class CreateAccountHandler(MainHandler):
             confirm_password = self.request.get("confirm_password")
             email = self.request.get("email")
             confirm_email = self.request.get("confirm_email")
+            data = [username, password, confirm_password, email, confirm_email]
+            
+            log_args(data)
 
-            if validAccountData(username, password, confirm_password, email, confirm_email):
-                self.write("storing user in db")
-            else:
-                self.write("bad data")
+            # account_check_response = analyze_form(username, password, confirm_password, email, confirm_email):
+            # if account_check_response[0] == True:
+            #     self.write("user with username %s" % username + " stored successfully")
+            #     log("user stored sanity check")
+
+            # elif account_check_response[0] == False:
+            #     self.write("Form Error: %s" % account_check_response[1])
+
         except Exception as e:
             self.write("Exception Encountered while validating form: %s" % str(e))
+
+# def analyze_form(username, password, confirm_password, email, confirm_email):
+#     error_msg = None
+#     if len(username) >= 2:
+#         if len(password) >=5:
+#             if validEmail(email):
+#                 if passwords_match(password, confirm_password)
+#                     if emails_match(email, confirm_email):
+#                         // add exists method here
+#                         try:
+#                             OneFeedUser.store_user(username, password, confirm_password, email, confirm_email)
+#                             return (True, error_msg)
+
+#                         except  Exception as e:
+#                             log("Exception Encountered: %s" % str(e))
+#                     else:
+#                         error_msg = "emails do not match"
+#                 else:
+#                     error_msg = "passwords do not match"
+#             else:
+#                 error_msg = "invalid email"
+#         else:
+#             error_msg = "password must be at least 5 characters long"
+#     else:
+#         error_msg = "username must be at least 2 characters long"
+
+#     return (False, error_msg)
+
+def emails_match(email, confirm_email):
+    return validEmail(email) and validEmail(confirm_email) and email == confirm_email
+
+def validEmail(email):
+    match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
+    if match != None:
+        return True
 
 def log(msg):
     logging.info(msg)
 
-def validAccountData(username, password, confirm_password, email, confirm_email):
-    error = False
-    error_msg = ""
-    if len(username) >= 2:
-        if len(password) >=5:
-            if validEmail(email):
-                if passwords_match(password, confirm_password)
-                    if emails_match(email, confirm_email):
-                
-
-    return True
+def log_args(args):
+    for arg in args:
+        log(arg)
 
 app = webapp2.WSGIApplication([
     ('/create_account', CreateAccountHandler),
